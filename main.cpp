f9a9756caae4c3c591e8cabb39a9ab5f0c086208
lamsi <islamchafabentayeb@gmail.com>
2018-03-27 11:01:36 +0200

    copy past code from halim

--- main.cpp
+++ main.cpp
@@ -8,18 +8,6 @@
 void GmsMatch(Mat &img1, Mat &img2);
 
 void runImagePair(){
-    Mat img1 = imread("../data/nn_left.jpg");
-    Mat img2 = imread("../data/nn_right.jpg");
-
-    imresize(img1, 480);
-    imresize(img2, 480);
-
-    GmsMatch(img1, img2);
-}
-
-
-int main()
-{
 
     std::string best_matche, j, out_capt_time, in_capt_time;
     int hN, mN, sN, hI, mI, sI, hD, mD, sD, TimeDifference, matches(0), max_matches(0);
@@ -28,12 +16,78 @@ int main()
     FILE *f;
 
     // get capture time of the output image
-
+string date1;
     f=popen("date |cut -d ' ' -f5","r");
-    if(fgets(data,256,f)!=NULL) date1.append(data);pclose(f);
-    hN=std::stoi(date1.substr(0,2));
-    mN=std::stoi(date1.substr(3,2));
-    sN=std::stoi(date1.substr(6,2));
+    if(fgets(data,256,f)!=NULL) out_capt_time.append(data);pclose(f);
+    hN=std::stoi(out_capt_time.substr(0,2));
+    mN=std::stoi(out_capt_time.substr(3,2));
+    sN=std::stoi(out_capt_time.substr(6,2));
+
+    //creation d'un fichier contenant tous les noms des images déjà enregistrées
+
+    system("ls -1 /home/halim/darknet-3/darknet/img/*.jpg | cut -d/ -f7 | sort -n>/home/halim/darknet-3/darknet/img/ImageNames.txt");
+    ifstream monflux("/home/halim/darknet-3/darknet/img/ImageNames.txt");
+    Mat img_out = imread("/home/halim/darknet-3/darknet/img/31.jpg");
+
+    while(getline(monflux,j))
+    {
+        j = "/home/lamsi/Documents/darknet/img/" + j ;
+        Mat img_in = imread(j);
+        imresize(img_out, 480);
+        imresize(img_in, 480);
+
+        matches = GmsMatch(img_out, img_in);
+        if(matches>max_matches)
+        {
+            max_matches=  matches;       //recupérer le nombre maximum de matches
+            best_matche = j;              //enregistrer le nom d'image avec le maximum de matches
+        }
+    }
+
+    // Récupération de la date de création de l'image
+
+    string commande="date -r " + ImageMAtch + "| cut -d ' ' -f5";
+    f=popen(commande.c_str(),"r");
+    if(fgets(data,256,f)!=NULL) in_capt_time.append(data);
+    pclose(f);
+    hI = std::stoi(in_capt_time.substr(0,2));
+    mI = std::stoi(in_capt_time.substr(3,2));
+    sI = std::stoi(in_capt_time.substr(6,2));
+
+    cout << "heure actuelle est: " << hN << endl;
+    cout << "minute actuelle est: " << mN << endl;
+    cout << "seconde actuelle est: " << sN << endl;
+    cout << " heure de création de l image est :"<< hI <<endl;
+    cout << " minute de création est "<< mI << endl;
+    cout << " seconde de création est "<< sI <<endl;
+
+    cout << "----------------------------------------" << endl;
+    cout << "le maximum des matches =" << max_matches<<endl;
+    cout << "le nom d'image avec le plus de matches est" << ImageMAtch << endl;
+    sD = sN - sI;
+    if(sD<0)
+    {
+        sD += 60;
+        mN -= 1;
+    }
+    mD = mN - mI;
+    if(mD<0)
+    {
+        mD += 60;
+        hN -= 1;
+    }
+    hD=hN-hI;
+    if(hD<0)
+        hD += 24;
+    TimeDifference = (hD*3600) + (mD*60) + sD;
+
+    cout << "le temps entre l'entrée et la sortie de la voiture est: " << TimeDifference << " secondes" << endl;
+
+}
+
+
+int main()
+{
 
 #ifdef USE_GPU
     int flag = cuda::getCudaEnabledDeviceCount();
